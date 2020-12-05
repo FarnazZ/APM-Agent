de0b51cc70758a610b3e66597b843426d77d435f
FarnazZ <39217806+FarnazZ@users.noreply.github.com>
2020-12-05 14:15:04 +0330

    Update node.js

--- node.js
+++ node.js
@@ -4,7 +4,7 @@ const apm = require('elastic-apm-node').start({
   //   // Allowed characters: a-z, A-Z, 0-9, -, _, and space
   serviceName: 'apm-server-sample-apm-http',
   // Use if APM Server requires a token
-  secretToken: 'T072ukv0y30zmBBZ8va08H2u',
+  secretToken: 'Bfi9plmPW4a1h3f997Ur529s',
   // Set custom APM Server URL (default: http://localhost:8200)
   serverUrl: 'http://apm-server-sample-apm-http.eck.svc:8200',
   verifyServerCert: 'false',
